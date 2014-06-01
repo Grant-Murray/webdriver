6b3506506adb7b70c75c8dccf9f7a4729f00006b
Grant Murray <email@grantmurray.com>
2014-06-01 07:22:44 -0700

    Changed some of the page waits

--- plog/login_test.go
+++ plog/login_test.go
@@ -1,57 +1,57 @@
 package plog
 
 import (
-	"fmt"
-	"github.com/Grant-Murray/webdriver"
-	"github.com/sourcegraph/go-selenium"
-	"testing"
-	"time"
+  "fmt"
+  "github.com/Grant-Murray/webdriver"
+  "github.com/sourcegraph/go-selenium"
+  "testing"
+  "time"
 )
 
 type Login struct {
-	UserIdentifier string
-	ClearPassword  string
+  UserIdentifier string
+  ClearPassword  string
 }
 
 func ExpectOnLoginPage(t *testing.T) {
 
-	webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
+  webdriver.WaitFor(5*time.Second, webdriver.ElementToAppear, "form[name=\"loginForm\"]")
+  if webdriver.WaitForTimedOut {
+    t.Fatalf("Expected to be on the login page, but timed out waiting for it")
+  }
 
-	_, err := webdriver.Drv.FindElement(selenium.ByCSSSelector, "[name=\"loginForm\"]")
-	if err != nil {
-		t.Fatalf("Expected to be on the login page, but finding loginForm: %s", err)
-	}
+  // it may not be fully loaded yet ...
 
 }
 
 func GotoLogin(t *testing.T) {
-	err := webdriver.Drv.Get(`https://plog.org:8004/`)
-	if err != nil {
-		t.Fatalf("Goto login failed: %s", err)
-	}
+  err := webdriver.Drv.Get(`https://plog.org:8004/`)
+  if err != nil {
+    t.Fatalf("Goto login failed: %s", err)
+  }
 }
 
 func SubmitLogin(in Login, t *testing.T) {
 
-	// Get elements
-	elements, err := webdriver.FindNamedElements([]string{"UserIdentifier", "ClearPassword", "LoginButton"})
-	if err != nil {
-		t.Fatalf("FindNamedElements failed: %s", err)
-	}
+  // Get elements
+  elements, err := webdriver.FindNamedElements([]string{"UserIdentifier", "ClearPassword", "LoginButton"})
+  if err != nil {
+    t.Fatalf("FindNamedElements failed: %s", err)
+  }
 
-	// Fill in
-	elements["UserIdentifier"].Clear()
-	elements["UserIdentifier"].SendKeys(in.UserIdentifier)
-	elements["ClearPassword"].Clear()
-	elements["ClearPassword"].SendKeys(in.ClearPassword)
+  // Fill in
+  elements["UserIdentifier"].Clear()
+  elements["UserIdentifier"].SendKeys(in.UserIdentifier)
+  elements["ClearPassword"].Clear()
+  elements["ClearPassword"].SendKeys(in.ClearPassword)
 
-	// Submit
-	elements["LoginButton"].Click()
+  // Submit
+  elements["LoginButton"].Click()
 }
 
 func checkSessionToken(t *testing.T) bool {
 
-	js := `
+  js := `
     var $injector = angular.injector(['ngCookies']);
     return $injector.invoke(function($cookies) {
       if ($cookies.SessionToken && ($cookies.SessionToken.length == 36)) {
@@ -62,200 +62,219 @@ func checkSessionToken(t *testing.T) bool {
     });
     `
 
-	// check for SessionToken - it should be present if we logged in successfully, absent otherwise
-	result, err := webdriver.Drv.ExecuteScript(js, nil)
-	if err != nil {
-		t.Fatalf("Failed to ExecuteScript: %s", err)
-	} else if result != "present" {
-		return false
-	}
-	return true
+  // check for SessionToken - it should be present if we logged in successfully, absent otherwise
+  result, err := webdriver.Drv.ExecuteScript(js, nil)
+  switch {
+  case err != nil:
+    t.Fatalf("Failed to ExecuteScript: %s", err)
+  case result == "present":
+    return true
+  case result == "absent":
+    return false
+  default:
+    t.Fatalf("checkSessionToken yielded unexpected result: %s", result)
+  }
+  return false
 }
 
 func ExpectSessionToken(t *testing.T) {
-	if !checkSessionToken(t) {
-		t.Fatalf("SessionToken expected to be present but was missing")
-	}
+  if !checkSessionToken(t) {
+    t.Fatalf("SessionToken expected to be present but was missing")
+  }
 }
 
 func ExpectNoSessionToken(t *testing.T) {
-	if checkSessionToken(t) {
-		t.Fatalf("SessionToken expected to be absent but was found")
-	}
+  if checkSessionToken(t) {
+    t.Fatalf("SessionToken expected to be absent but was found")
+  }
 }
 
 const (
-	LogoutPageUrl = "https://plog.org:8004/#/album"
+  LogoutPageUrl = "https://plog.org:8004/#/album"
 )
 
 func Test_Login_Setup(t *testing.T) {
-	if err := webdriver.InitializeRemote(); err != nil {
-		t.Fatalf("Cannot connect to selenium server: %s", err)
-	}
+  if err := webdriver.InitializeRemote(); err != nil {
+    t.Fatalf("Cannot connect to selenium server: %s", err)
+  }
 }
 
 type loginCase struct {
-	idTyp string
-	lin   Login
-	msg   string
-	tokc  string
+  idTyp string
+  lin   Login
+  msg   string
+  tokc  string
 }
 
 func Test_Login_table(t *testing.T) {
 
-	cases := []loginCase{
-		{"UserId", Login{"no such dude", "passwordpassword"}, "Authentication failed", "absent"},
-		{"UserId", Login{"wrong uid", userOne.ClearPassword}, "Authentication failed", "absent"},
-		{"UserId", Login{userOne.UserId, "wrong password"}, "Authentication failed", "absent"},
-		{"UserId", Login{userOne.UserId, userOne.ClearPassword}, "SKIP", "present"},
-		// if already logged in, automatic login will happen. So for testing should log out
-	}
+  cases := []loginCase{
+    {"UserId", Login{"no such dude", "passwordpassword"}, "Authentication failed", "absent"},
+    {"UserId", Login{"wrong uid", userOne.ClearPassword}, "Authentication failed", "absent"},
+    {"UserId", Login{userOne.UserId, "wrong password"}, "Authentication failed", "absent"},
+    {"UserId", Login{userOne.UserId, userOne.ClearPassword}, "SKIP", "present"},
+    // if already logged in, automatic login will happen. So for testing should log out
+  }
 
-	for c := 0; c < len(cases); c++ {
-		cur := cases[c]
-		t.Logf("Case %d: UserIdentifier (%s)=%s ClearPassword=%s", c, cur.idTyp, cur.lin.UserIdentifier, cur.lin.ClearPassword)
+  for c := 0; c < len(cases); c++ {
+    cur := cases[c]
+    t.Logf("Case %d: UserIdentifier (%s)=%s ClearPassword=%s", c, cur.idTyp, cur.lin.UserIdentifier, cur.lin.ClearPassword)
 
-		if len(cur.lin.ClearPassword) < 10 {
-			t.Fatalf("Case is not valid since ClearPassword (%s) is too short")
-		}
+    if len(cur.lin.ClearPassword) < 10 {
+      t.Fatalf("Case is not valid since ClearPassword (%s) is too short")
+    }
 
-		GotoLogin(t)
-		ExpectOnLoginPage(t)
-		SubmitLogin(cur.lin, t)
+    GotoLogin(t)
+    ExpectOnLoginPage(t)
+    SubmitLogin(cur.lin, t)
 
-		webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
+    webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
 
-		if cur.tokc == "absent" {
-			// Login failure was expected
-			msg, err := webdriver.FetchText("p[name='LoginMessage']")
-			if err != nil {
-				t.Fatalf("Failed to fetch the main message: %s", err)
-			}
+    if cur.tokc == "absent" {
+      // Login failure was expected
+      msg, err := webdriver.FetchText("p[name='LoginMessage']")
+      if err != nil {
+        t.Fatalf("Failed to fetch the main message: %s", err)
+      }
 
-			if msg != "Authentication failed" {
-				t.Errorf("Expected failure message, got \"%s\"", msg)
-			}
+      if msg != "Authentication failed" {
+        t.Errorf("Expected failure message, got \"%s\"", msg)
+      }
 
-			ExpectNoSessionToken(t)
-		} else {
-			ExpectSessionToken(t)
-		}
-	}
+      ExpectNoSessionToken(t)
+    } else {
+      ExpectSessionToken(t)
+    }
+  }
 }
 
 func Logout(t *testing.T) {
 
-	ExpectSessionToken(t)
+  ExpectSessionToken(t)
 
-	err := webdriver.Drv.Get(LogoutPageUrl)
-	if err != nil {
-		t.Fatalf("Failed to load %s: %s\n", LogoutPageUrl, err)
-	}
-	webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
+  err := webdriver.Drv.Get(LogoutPageUrl)
+  if err != nil {
+    t.Fatalf("Failed to load %s: %s\n", LogoutPageUrl, err)
+  }
+  webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
 
-	sel := "a[name=\"Logout\"]"
-	LogoutLink, err := webdriver.Drv.FindElement(selenium.ByCSSSelector, sel)
-	if err != nil {
-		t.Fatalf("Failed to find element %s (%s)\n", sel, err)
-	}
+  sel := "[name=\"Logout\"]"
+  LogoutLink, err := webdriver.Drv.FindElement(selenium.ByCSSSelector, sel)
+  if err != nil {
+    t.Fatalf("Failed to find element %s (%s)\n", sel, err)
+  }
 
-	LogoutLink.Click()
-	webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
-
-	ExpectNoSessionToken(t)
+  LogoutLink.Click() // BUG ng-click with href="" fails under selenium
+  ExpectOnLoginPage(t)
+  ExpectNoSessionToken(t)
 }
 
 func Test_Logout(t *testing.T) {
-	// happy path logout
-	t.Logf("Case: happy path logout")
-	Logout(t)
+  // happy path logout
+  t.Logf("Case: happy path logout")
+  Logout(t)
 
-	// visit while logged out; expect to get login page
-	// TODO add an Image URL
-	pagesNeedingLogin := []string{"/#/album", "/#/album/2013-09%20September"}
+  // visit while logged out; expect to get login page
+  // TODO add an Image URL
+  pagesNeedingLogin := []string{"/#/album", "/#/album/2013-09%20September"}
 
-	for p := 0; p < len(pagesNeedingLogin); p++ {
-		page := fmt.Sprintf("https://plog.org:8004%s", pagesNeedingLogin[p])
-		t.Logf("Case: %s", pagesNeedingLogin[p])
+  for p := 0; p < len(pagesNeedingLogin); p++ {
+    page := fmt.Sprintf("https://plog.org:8004%s", pagesNeedingLogin[p])
+    t.Logf("Case: %s", pagesNeedingLogin[p])
 
-		err := webdriver.Drv.Get(page)
-		if err != nil {
-			t.Fatalf("Failed to load %s: %s\n", page, err)
-		}
+    err := webdriver.Drv.Get(page)
+    if err != nil {
+      t.Fatalf("Failed to load %s: %s\n", page, err)
+    }
 
-		webdriver.WaitFor(5*time.Second, webdriver.UrlIsCurrent, "https://plog.org:8004/#/login")
-		if webdriver.WaitForTimedOut {
-			t.Fatalf("Failed to land at expected URL")
-		}
+    webdriver.WaitFor(5*time.Second, webdriver.UrlIsCurrent, "https://plog.org:8004/#/login")
+    if webdriver.WaitForTimedOut {
+      t.Fatalf("Failed to land at expected URL")
+    }
 
-		msg, err := webdriver.FetchText("p[name='LoginMessage']")
-		if err != nil {
-			t.Fatalf("Failed to fetch the main message: %s", err)
-		}
+    msg, err := webdriver.FetchText("p[name='LoginMessage']")
+    if err != nil {
+      t.Fatalf("Failed to fetch the main message: %s", err)
+    }
 
-		if msg != "" {
-			t.Errorf("Message was not blank: %s", msg)
-		}
+    if msg != "" {
+      t.Errorf("Message was not blank: %s", msg)
+    }
 
-		result, err := webdriver.Drv.ExecuteScript("return localStorage['SessionToken']", nil)
-		if err != nil {
-			t.Fatalf("Failed to ExecuteScript: %s", err)
-		}
+    result, err := webdriver.Drv.ExecuteScript("return localStorage['SessionToken']", nil)
+    if err != nil {
+      t.Fatalf("Failed to ExecuteScript: %s", err)
+    }
 
-		if result != nil {
-			t.Errorf("Found a SessionToken in localStorage: %s", result)
-		}
+    if result != nil {
+      t.Errorf("Found a SessionToken in localStorage: %s", result)
+    }
 
-	}
+  }
 
-	// automatic login when SessionToken is still valid: goto login, then autologin and end up on album page
-	t.Logf("Case: Automatic login that works")
-	GotoLogin(t)
-	ExpectOnLoginPage(t)
-	SubmitLogin(Login{userOne.UserId, userOne.ClearPassword}, t)
+  // automatic login when SessionToken is still valid: goto login, then autologin and end up on album page
+  t.Logf("Case: Automatic login that works")
+  GotoLogin(t)
+  ExpectOnLoginPage(t)
+  SubmitLogin(Login{userOne.UserId, userOne.ClearPassword}, t)
 
-	webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
+  webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
 
-	ExpectSessionToken(t)
-	GotoLogin(t)
+  ExpectSessionToken(t)
 
-	webdriver.WaitFor(5*time.Second, webdriver.UrlIsCurrent, `https://plog.org:8004/#/`)
-	if webdriver.WaitForTimedOut {
-		t.Fatalf("Failed to land at expected URL")
-	}
+  // need to visit a page that needs login - the LogoutPageUrl page needs login
+  err := webdriver.Drv.Get(LogoutPageUrl)
+  if err != nil {
+    t.Fatalf("Failed to load %s: %s\n", LogoutPageUrl, err)
+  }
 
-	// automatic login when SessionToken is present but not valid
-	t.Logf("Case: Automatic login with bad session token")
-	_, err := webdriver.Drv.ExecuteScript("localStorage['SessionToken'] = '00000000-0000-0000-dead-beef00000000'", nil)
-	if err != nil {
-		t.Fatalf("Failed to ExecuteScript: %s", err)
-	}
+  webdriver.WaitFor(5*time.Second, webdriver.UrlIsCurrent, LogoutPageUrl)
+  if webdriver.WaitForTimedOut {
+    t.Fatalf("Failed to land at expected URL")
+  }
 
-	GotoLogin(t)
+  // automatic login when SessionToken is present but not valid
+  t.Logf("Case: Automatic login with bad session token")
 
-	webdriver.WaitFor(5*time.Second, webdriver.ElementToVanish, "div[class='selenium-flag']")
-	if webdriver.WaitForTimedOut {
-		t.Fatalf("Failed to end up at login page")
-	}
+  /* This angular method does not work, need to use the document.cookie method  :(
+  js := `
+    var $injector = angular.injector(['ngCookies']);
+    return $injector.invoke(function($cookies) {
+      $cookies.SessionToken = '00000000-0000-0000-dead-beef00000000';
+    });
+    `
+  _, err = webdriver.Drv.ExecuteScript(js, nil)
+  */
 
-	ExpectOnLoginPage(t)
+  _, err = webdriver.Drv.ExecuteScript(`document.cookie = 'SessionToken=00000000-0000-0000-dead-beef00000000';`, nil)
+  if err != nil {
+    t.Fatalf("Failed to ExecuteScript: %s", err)
+  }
 
-	msg, err := webdriver.FetchText("p[name='LoginMessage']")
-	if err != nil {
-		t.Fatalf("Failed to fetch the login message: %s", err)
-	}
+  // need to visit a page that needs login - the LogoutPageUrl page needs login
+  err = webdriver.Drv.Get(LogoutPageUrl)
+  if err != nil {
+    t.Fatalf("Failed to load %s: %s\n", LogoutPageUrl, err)
+  }
 
-	if msg != "" {
-		// automatic logins don't produce "Authentication failed" messages
-		t.Errorf("Expected LoginMessage to be blank, got \"%s\"", msg)
-	}
+  ExpectOnLoginPage(t)
 
-	// TODO periodic re-login to extend session : Problem is how to test this without waiting 5 minutes
+  msg, err := webdriver.FetchText("p[name='LoginMessage']")
+  if err != nil {
+    t.Fatalf("Failed to fetch the login message: %s", err)
+  }
 
-	// TODO login, visit page that calls loggedIn() - delete session in background - visit page that calls loggedIn() + expect it to return true - wait for background login attempt - visit page that calles loggedIn() + expect it to return false
+  if msg != "" {
+    // automatic logins don't produce "Authentication failed" messages
+    t.Errorf("Expected LoginMessage to be blank, got \"%s\"", msg)
+  }
+
+  // TODO periodic re-login to extend session : Problem is how to test this without waiting 5 minutes
+
+  // TODO login, visit page that calls loggedIn() - delete session in background - visit page that calls loggedIn() + expect it to return true - wait for background login attempt - visit page that calles loggedIn() + expect it to return false
 
 }
 
 func Test_Login_Breakdown(t *testing.T) {
-	webdriver.Drv.Quit()
+  webdriver.Drv.Quit()
 }
--- webdriver.go
+++ webdriver.go
@@ -2,16 +2,16 @@
 package webdriver
 
 import (
-	"fmt"
-	"github.com/sourcegraph/go-selenium"
-	"io/ioutil"
-	"time"
+  "fmt"
+  "github.com/sourcegraph/go-selenium"
+  "io/ioutil"
+  "time"
 )
 
 // globals
 var (
-	Drv             selenium.WebDriver
-	WaitForTimedOut bool
+  Drv             selenium.WebDriver
+  WaitForTimedOut bool
 )
 
 const RemoteURL = "http://localhost:4444/wd/hub"
@@ -19,123 +19,123 @@ const InitialWait time.Duration = 500 * time.Millisecond
 
 // InitializeRemote establishes the connection to the remote selenium server
 func InitializeRemote() (err error) {
-	// GLM(self) run /opt/selenium/start-server.sh to start the server
-	caps := selenium.Capabilities(map[string]interface{}{"browserName": "chrome"})
-	if Drv, err = selenium.NewRemote(caps, RemoteURL); err != nil {
-		err = fmt.Errorf("Failure calling selenium.NewRemote for %s: %s\n", RemoteURL, err)
-	}
-	return err
+  // GLM(self) run /opt/selenium/start-server.sh to start the server
+  caps := selenium.Capabilities(map[string]interface{}{"browserName": "chrome"})
+  if Drv, err = selenium.NewRemote(caps, RemoteURL); err != nil {
+    err = fmt.Errorf("Failure calling selenium.NewRemote for %s: %s\n", RemoteURL, err)
+  }
+  return err
 }
 
 // ScreenshotToFile takes a screenshot and writes it to filename
 func ScreenshotToFile(filename string) (err error) {
 
-	screenshot, err := Drv.Screenshot()
-	if err != nil {
-		err = fmt.Errorf("Error during ScreenshotToFile using filename %s\n  Error:%s\n", filename, err)
-		return err
-	} else {
-		ioutil.WriteFile(filename, screenshot, 0644)
-	}
-	return nil
+  screenshot, err := Drv.Screenshot()
+  if err != nil {
+    err = fmt.Errorf("Error during ScreenshotToFile using filename %s\n  Error:%s\n", filename, err)
+    return err
+  } else {
+    ioutil.WriteFile(filename, screenshot, 0644)
+  }
+  return nil
 }
 
 // ElementToVanish is a WaitFor function. As long as the element is present
 // waiting continues, once the element cannot be found waiting stops
 func ElementToVanish(sel []interface{}) bool {
-	_, err := Drv.FindElement(selenium.ByCSSSelector, sel[0].(string))
-	if err.Error() == "no such element" {
-		return true
-	}
-	return false
+  _, err := Drv.FindElement(selenium.ByCSSSelector, sel[0].(string))
+  if err.Error() == "no such element" {
+    return true
+  }
+  return false
 }
 
 // ElementToAppear is a WaitFor function. As long as the element is absent
 // waiting continues, once the element is found waiting stops
 func ElementToAppear(sel []interface{}) bool {
-	_, err := Drv.FindElement(selenium.ByCSSSelector, sel[0].(string))
-	if err == nil {
-		return true
-	}
-	return false
+  _, err := Drv.FindElement(selenium.ByCSSSelector, sel[0].(string))
+  if err == nil {
+    return true
+  }
+  return false
 }
 
 // DocumentIsReady can be used as a WaitFor isReady parameter
 func DocumentIsReady(unused []interface{}) bool {
-	result, err := Drv.ExecuteScript("return document.readyState", nil)
-	if err != nil && result == "complete" {
-		return true
-	}
-	return false
+  result, err := Drv.ExecuteScript("return document.readyState", nil)
+  if err != nil && result == "complete" {
+    return true
+  }
+  return false
 }
 
 // UrlIsCurrent can be used as a WaitFor isReady parameter
 func UrlIsCurrent(urls []interface{}) bool {
-	cur, err := Drv.CurrentURL()
-	if err != nil {
-		return false
-	}
+  cur, err := Drv.CurrentURL()
+  if err != nil {
+    return false
+  }
 
-	for i := 0; i < len(urls); i++ {
-		if cur == urls[i].(string) {
-			return true
-		}
-	}
-	return false
+  for i := 0; i < len(urls); i++ {
+    if cur == urls[i].(string) {
+      return true
+    }
+  }
+  return false
 }
 
 // WaitFor sleeps until isReady() returns true unless it waits as long as timeoutAfter then it sets WaitForTimedOut to true and returns
 func WaitFor(timeoutAfter time.Duration, isReady func([]interface{}) bool, args ...interface{}) {
-	time.Sleep(InitialWait)
+  time.Sleep(InitialWait)
 
-	if InitialWait >= timeoutAfter {
-		WaitForTimedOut = true
-		return
-	}
+  if InitialWait >= timeoutAfter {
+    WaitForTimedOut = true
+    return
+  }
 
-	const ITERATIONS = 10
-	var sleepDuration time.Duration = (timeoutAfter - InitialWait) / ITERATIONS
+  const ITERATIONS = 10
+  var sleepDuration time.Duration = (timeoutAfter - InitialWait) / ITERATIONS
 
-	for i := 0; i <= ITERATIONS; i++ {
-		if isReady(args) {
-			WaitForTimedOut = false
-			return
-		}
-		time.Sleep(sleepDuration)
-	}
+  for i := 0; i <= ITERATIONS; i++ {
+    if isReady(args) {
+      WaitForTimedOut = false
+      return
+    }
+    time.Sleep(sleepDuration)
+  }
 
-	WaitForTimedOut = true
+  WaitForTimedOut = true
 }
 
 // FindNamedElements returns a map of elements with a memeber for each name in names
 func FindNamedElements(names []string) (elements map[string]selenium.WebElement, err error) {
 
-	elements = make(map[string]selenium.WebElement, 100)
+  elements = make(map[string]selenium.WebElement, 100)
 
-	for _, n := range names {
-		sel := fmt.Sprintf("[name=\"%s\"]", n)
-		elements[n], err = Drv.FindElement(selenium.ByCSSSelector, sel)
-		if err != nil {
-			err = fmt.Errorf("Error finding element %s: %s", sel, err)
-			return elements, err
-		}
-	}
-	return elements, nil
+  for _, n := range names {
+    sel := fmt.Sprintf("[name=\"%s\"]", n)
+    elements[n], err = Drv.FindElement(selenium.ByCSSSelector, sel)
+    if err != nil {
+      err = fmt.Errorf("Error finding element %s: %s", sel, err)
+      return elements, err
+    }
+  }
+  return elements, nil
 }
 
 // FetchText returns the msg text in an element ByCSSSelector sel
 func FetchText(sel string) (msg string, err error) {
-	var e selenium.WebElement
+  var e selenium.WebElement
 
-	e, err = Drv.FindElement(selenium.ByCSSSelector, sel)
-	if err != nil {
-		err = fmt.Errorf("Failed to find element %s (%s)\n", sel, err)
-		return "", err
-	}
-	msg, err = e.Text()
-	if err != nil {
-		err = fmt.Errorf("Failed to retrieve %s text: %s", sel, err)
-		return "", err
-	}
-	return msg, nil
+  e, err = Drv.FindElement(selenium.ByCSSSelector, sel)
+  if err != nil {
+    err = fmt.Errorf("Failed to find element %s (%s)\n", sel, err)
+    return "", err
+  }
+  msg, err = e.Text()
+  if err != nil {
+    err = fmt.Errorf("Failed to retrieve %s text: %s", sel, err)
+    return "", err
+  }
+  return msg, nil
 }
